f771ce024f9ae0b9ceb3074f2637073d27f77aa0
Fakhreddine Bouaziz <71568228+Fakhreddineuottawa@users.noreply.github.com>
2021-06-27 07:03:19 -0700

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text,'lxml')
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for company in jobs :
+    company_name = company.find('h3', class_="joblist-comp-name").text.replace(' ', '')
+    print(company_name)
